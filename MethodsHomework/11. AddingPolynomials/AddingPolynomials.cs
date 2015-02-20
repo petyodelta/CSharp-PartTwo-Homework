4eeb80822290f00fd34e9940d2102e5c88f94b2f
petyodelta <petyodelta@gmail.com>
2015-02-20 20:50:35 +0200

    Edit 11.AddPolynominals Added 12.SutractPolynominals

--- MethodsHomework/11. AddingPolynomials/AddingPolynomials.cs
+++ MethodsHomework/11. AddingPolynomials/AddingPolynomials.cs
@@ -11,54 +11,96 @@ class AddingPolynomials
 {
     static void Main()
     {
-        Console.Write("Enter polynominal degree: ");
-        int degree = int.Parse(Console.ReadLine());
-        
-        int[] array1 = new int[degree +1];
+        Console.Write("Enter first polynominal degree: ");
+        int degreeFirstPol = int.Parse(Console.ReadLine());
+
+        int[] array1 = new int[degreeFirstPol + 1];
         Console.WriteLine("Enter first polynominal:");
-        for (int i = degree; i >= 0; i--)
+        for (int i = degreeFirstPol; i >= 0; i--)
         {
             Console.Write("x^{0}=", i);
             array1[i] = int.Parse(Console.ReadLine());
         }
         
-        int[] array2 = new int[degree + 1];
+        Console.Write("Enter second polynominal degree: ");
+        int degreeSecondPol = int.Parse(Console.ReadLine());
+
+        int[] array2 = new int[degreeSecondPol + 1];
         Console.WriteLine("Enter second polynominal:");
-        for (int i = degree; i >= 0; i--)
+        for (int i = degreeSecondPol; i >= 0; i--)
         {
             Console.Write("x^{0}=", i);
             array2[i] = int.Parse(Console.ReadLine());
         }
-
+        int degree = 0;
+        if (degreeFirstPol >= degreeSecondPol)
+        {
+            degree = degreeFirstPol;
+        }
+        else if (degreeSecondPol > degreeFirstPol)
+        {
+            degree = degreeSecondPol;
+        }
         int[] arraySum = AddPolynominals(array1, array2);
         
         //output
+        Print(arraySum, degree);
         
+    }
+    static int[] AddPolynominals(int[] array1, int[] array2)
+    {
+
+        int[] arraySum = new int[Math.Max(array1.Length, array2.Length)];
+        
+        for (int i = 0; i < arraySum.Length; i++)
+        {
+            if (i < Math.Min(array1.Length, array2.Length))
+            {
+                arraySum[i] = array1[i] + array2[i];
+            }
+            else
+            {
+                if (array1.Length > array2.Length)
+                {
+                    arraySum[i] = array1[i];
+                }
+                else
+                {
+                    arraySum[i] = array2[i];
+                }
+            }
+        }
+        Array.Reverse(arraySum);
+        return arraySum;
+    }
+
+    static void Print(int[] arraySum, int degree)
+    {
         for (int i = 0; i < arraySum.Length; i++)
         {
             if (i == arraySum.Length - 1)
             {
-                Console.Write("{0}",arraySum[i]);
+                if (arraySum[i] != 0)
+                {
+                    Console.Write("+{0}", arraySum[i]);
+                }
+                Console.WriteLine();
             }
             else
             {
-                Console.Write("{0}x^{1}+", arraySum[i], degree);
+                if (arraySum[i] != 0 && i < arraySum.Length - 2)
+                {
+                    Console.Write("{0}x^{1}+", arraySum[i], degree);
+                }
+                else
+                {
+                    Console.Write("{0}x^{1}", arraySum[i], degree);
+                }
+
                 --degree;
             }
-            
-        }
 
-    }
-    static int[] AddPolynominals(int[] array1, int[] array2)
-    {
-        Array.Reverse(array1);
-        Array.Reverse(array2);
-        int[] arraySum = new int[array1.Length];
-        for (int i = 0; i < arraySum.Length; i++)
-        {
-            arraySum[i] = array1[i] + array2[i];
         }
-        return arraySum;
     }
 }
 
---
+++ MethodsHomework/12. SubtractingPolynomials/SubtractingPolynomials.cs
@@ -0,0 +1,126 @@
+﻿//Extend the previous program to support also subtraction and multiplication of polynomials.
+
+
+using System;
+
+class SubtractingPolynomials
+{
+    static void Main()
+    {
+        Console.Write("Enter first polynominal degree: ");
+        int degreeFirstPol = int.Parse(Console.ReadLine());
+
+        int[] array1 = new int[degreeFirstPol + 1];
+        Console.WriteLine("Enter first polynominal:");
+        for (int i = degreeFirstPol; i >= 0; i--)
+        {
+            Console.Write("x^{0}=", i);
+            array1[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.Write("Enter second polynominal degree: ");
+        int degreeSecondPol = int.Parse(Console.ReadLine());
+
+        int[] array2 = new int[degreeSecondPol + 1];
+        Console.WriteLine("Enter second polynominal:");
+        for (int i = degreeSecondPol; i >= 0; i--)
+        {
+            Console.Write("x^{0}=", i);
+            array2[i] = int.Parse(Console.ReadLine());
+        }
+        int degree = 0;
+        if (degreeFirstPol >= degreeSecondPol)
+        {
+            degree = degreeFirstPol;
+        }
+        else if (degreeSecondPol > degreeFirstPol)
+        {
+            degree = degreeSecondPol;
+        }
+
+        int[] arraySubtract = SubtractPolynominals(array1, array2);
+
+        Console.WriteLine("Subtraction of polynominals");
+        Print(arraySubtract, degree);
+        
+        
+        int[] arrayMultiply = MultiplicationPolynominals(array1, array2);
+        degree = degreeFirstPol + degreeSecondPol;
+
+        Console.WriteLine("Multiplication of polynominals");
+        Print(arrayMultiply, degree);
+        
+    }
+    static int[] SubtractPolynominals(int[] array1, int[] array2)
+    {
+        int[] arraySubtract = new int[Math.Max(array1.Length, array2.Length)];
+        
+        for (int i = 0; i < arraySubtract.Length; i++)
+        {
+            if (i < Math.Min(array1.Length, array2.Length))
+            {
+                arraySubtract[i] = array1[i] - array2[i];    
+            }
+            else
+            {
+                if (array1.Length > array2.Length)
+                {
+                    arraySubtract[i] = array1[i];
+                }
+                else
+                {
+                    arraySubtract[i] = array2[i];
+                }
+            }
+            
+        }
+        Array.Reverse(arraySubtract);
+        return arraySubtract;
+    }
+    
+    static int[] MultiplicationPolynominals(int[] array1, int[] array2)
+    {
+        int[] arrayMultiply = new int[array1.Length + array2.Length -1];
+        for (int i = 0; i < array1.Length; i++)
+        {
+            for (int j = 0; j < array2.Length; j++)
+            {
+                arrayMultiply[i + j] += array1[i] * array2[j];
+            }
+        }
+        Array.Reverse(arrayMultiply);
+        return arrayMultiply;
+    }
+
+    static void Print(int[] result, int degree)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (i == result.Length - 1)
+            {
+                if (result[i] != 0)
+                {
+                    Console.Write("+({0})", result[i]);
+                }
+
+            }
+            else
+            {
+                if(result[i] != 0 && i < result.Length -2)
+                {
+                    Console.Write("({0}x^{1})+", result[i], degree);
+                }
+                else
+                {
+                    Console.Write("({0}x^{1})", result[i], degree);
+                }
+                --degree;
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
